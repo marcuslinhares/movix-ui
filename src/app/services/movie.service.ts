0a919d82cf8dd9af5221807d3fd7ee80c7be7c71
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-02 18:26:30 -0300

    feat: adiciona metodos de busca de filmes populares e montagem de url de imagem de filmes
    - adicionado configuração para desenvolvimento local no angular.json

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Movie } from '../models/movie.model';
+import { environment } from '../../environments/environment';
+import { MovieResponse } from '../models/DTOS/movie.response';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private readonly apiKey = environment.apiKey;
+  private readonly baseUrl = 'https://api.themoviedb.org/3';
+  private readonly baseImageUrl = 'https://image.tmdb.org/t/p/original';
+  private readonly LANGUAGE = 'pt-BR'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(page: number = 1): Observable<Movie[]> {
+    const url = `${this.baseUrl}/movie/popular`;
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('language', this.LANGUAGE)
+      .set('page', page.toString());
+
+    return this.http.get<MovieResponse>(url, { params }).pipe(
+      map(response => response.results)
+    );
+  }
+
+  getImageUrl(path: string): string {
+    return `${this.baseImageUrl}${path}`;
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiKey: 'API_KEY_PLACEHOLDER'
+};
