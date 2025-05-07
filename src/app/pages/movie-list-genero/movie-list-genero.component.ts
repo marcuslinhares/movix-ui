0db5880faa053d39ea3598afb84ed42ac2369882
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-07 15:10:37 -0300

    fix: corrige recarregamento de filmes ao trocar de genero na pagina de genero de filmes

--- src/app/pages/movie-list-genero/movie-list-genero.component.ts
+++ src/app/pages/movie-list-genero/movie-list-genero.component.ts
@@ -6,6 +6,8 @@ import { MovieCardComponent } from '../../components/movie-card/movie-card.compo
 import { MainLayoutComponent } from '../../layouts/main-layout/main-layout.component';
 import { CommonModule } from '@angular/common';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { GeneroService } from '../../services/genero.service';
+import { Genero } from '../../models/genero.model';
 
 @Component({
   selector: 'movie-list-genero',
@@ -17,20 +19,35 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 export class MovieListGeneroComponent implements OnInit {
   movies: Movie[] = [];
   genreId!: number;
+  generoName!: string;
   page = 1;
   isLoading = false;
   hasMore = true;
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: MovieService
+    private movieService: MovieService,
+    private generoService: GeneroService
   ) { }
 
   ngOnInit(): void {
-    this.genreId = +this.route.snapshot.paramMap.get('id')!;
-    this.loadMovies();
+    this.route.paramMap.subscribe(params => {
+      this.genreId = +params.get('id')!;
+      this.page = 1;
+      this.movies = [];
+      this.hasMore = true;
+      this.loadMovies();
+      this.loadGeneroName();
+    });
   }
 
+  loadGeneroName(): void {
+    this.generoService.getGeneros().subscribe((response) => {
+      const genero = response.genres.find(g => g.id === this.genreId);
+      this.generoName = genero?.name || '';
+    });
+  }
+  
   onScroll(): void {
     if (this.isLoading || !this.hasMore) return;
     this.page++;
